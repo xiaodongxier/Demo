4a30faade4059f005474e6abfb1a213d3d6dfa04
小东西儿 <56629378+xiaodongxier@users.noreply.github.com>
2021-05-14 22:28:56 +0800

    切换分支

---
+++ 慕课网/网课相关/瀑布流/js.js
@@ -0,0 +1,15 @@
+var note = {
+    init:function(){
+        this.iniDate()
+        this.a()
+    },
+    iniDate: function(){
+        var dom1 = this;
+        var dom = document.querySelector("div");
+        dom.innerHTML = "<div>hhhhhhhhhh</div>"
+    },
+    a: function(){
+        this.dom.innerHTML = "1"
+    }
+
+}
