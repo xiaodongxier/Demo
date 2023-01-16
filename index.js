3b64345ae0e0ff1b27c4f6b5252b5a5e545e7ed4
小东西儿 <xiaodongxier@gmail.com>
2023-01-16 13:28:09 +0800

    up

--- index.js
+++ index.js
@@ -1,7 +1,7 @@
 $(function(){
   var htmlList = ['基础HTML树视图'];
-  var cssCenter = ['基础CSS树视图美化'];
-  var jsRight = ['文字滑块颜色变化','文字弯曲弧度效果','jQuery全屏滚动插件fullPage.js演示','12306网站404页面时间处理方案','swiper层叠翻页效果','swiper层叠轮播效果'];
+  var cssCenter = ['基础CSS树视图美化','文字滑块颜色变化'];
+  var jsRight = ['文字弯曲弧度效果','jQuery全屏滚动插件fullPage.js演示','12306网站404页面时间处理方案','swiper层叠翻页效果','swiper层叠轮播效果'];
   for(var i=0;i<htmlList.length;i++){
     $(".htmlWrap").append(`<li><a href="./HTML/${htmlList[i]}" target="_blank" rel="noopener noreferrer">${htmlList[i]}</a></li>`)
   }
